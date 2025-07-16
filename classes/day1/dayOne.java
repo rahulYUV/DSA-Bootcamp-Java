099c65b4e0ad8b2994ac1eeca1ad2b354d9cee98
rahulYUV <rs6205903996@gmail.com>
2025-07-17 01:01:40 +0530

    moreAboutMemory

---
+++ classes/day1/dayOne.java
@@ -0,0 +1,35 @@
+public class dayOne{
+    public static void main(String[] args) {
+        // types of language 
+        // procedural 
+        // functional 
+        // object - oriendted 
+        
+        
+        // static and dynamic language 
+        /*
+         * STATIC = perform type checking at compile time 
+         * delclare dataytpe before you use it , we need to specifies the data type 
+         *  give more control 
+         * 
+         * DYNAMIC = perform type checking at run time 
+         * error will show till program runn 
+         * easy to write code , 
+         * save time 
+         * no need to declare the datatypes of variables 
+         *
+         */
+
+
+         // STACK and HEAP memory 
+         /*
+          * stack store the refernce pointing the object that is acutlly stored in heap 
+                more than one reference variable of one object 
+
+          */
+          // java follow pass by reference 
+          // it dosent have the pass by copy 
+          // garbage collection : none any reference pointyer is pointd toward this object 
+          
+    }
+}
